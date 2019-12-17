19dd770ebd9660e835c24e28e862cf32eaca6931
Juan Fumero <jjfumero@gmail.com>
2019-12-17 18:59:11 +0100

    Compilation script for Xilinx FPGAs

--- xilinxCompilation.sh
+++ xilinxCompilation.sh
@@ -1,14 +1,31 @@
 #!/usr/bin/env bash
 
-mkdir -p build
-cd build 
-
 PLATFORM=xilinx_kcu1500_dynamic_5_0
 
-## First compilation
-xocc -c -s -o interpreter.xo -t hw --platform $PLATFORM ../interpreter.cl
+function compileGlobalInterpreter {
+    mkdir -p buildGlobal
+    cd buildGlobal
+    xocc -c -s -o interpreter.xo -t hw --platform $PLATFORM ../interpreter.cl
+    xocc -l -s -O3 -j12 -o mykerinterpreternel.xclbin -t hw --platform $PLATFORM interpreter.xo
+    cd -
+}
 
-## Linking and bitstream generation
-xocc -l -s -o mykerinterpreternel.xclbin -t hw --platform $PLATFORM interpreter.xo
+function compileLocalInterpreter {
+    mkdir -p buildLocal
+    cd buildLocal
+    xocc -c -s -o interpreterLocal.xo -t hw --platform $PLATFORM ../interpreterLocal.cl
+    xocc -l -s -O3 -j12 -o mykerinterpreternelLocal.xclbin -t hw --platform $PLATFORM interpreterLocal.xo
+    cd -
+}
 
-cd -
+function compilePrivateInterpreter {
+    mkdir -p buildPrivate
+    cd buildPrivate
+    xocc -c -s -o interpreterPrivate.xo -t hw --platform $PLATFORM ../interpreterPrivate.cl
+    xocc -l -s -O3 -j12 -o mykerinterpreternelPrivate.xclbin -t hw --platform $PLATFORM interpreterPrivate.xo
+    cd -
+}
+
+compileGlobalInterpreter
+compileLocalInterpreter
+compilePrivateInterpreter
