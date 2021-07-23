0aab5d0e162e76559895d9e742f540c1d3460f7a
Juan Fumero <juan.fumero@manchester.ac.uk>
2021-07-23 09:28:57 +0100

    Runner script updated

--- run.sh
+++ run.sh
@@ -1,6 +1,15 @@
 #!/bin/env bash
 
+libOCL=lib
+if [ -L ${libOCL} ] ; then
+	if [ -e ${libOCL} ] ; then
+		echo "lib OK"
+	fi
+elif [ -e ${my_link} ] ; then
+	ln -s src/ lib
+fi
+
 inputSize=(32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288)
 for s in ${inputSize[@]}; do
-    ./bin/gpuBenchmark $s
+    ./build/bin/gpuBenchmark $s
 done
